84fdf2d61adcfdf2527469f04c4e9fb3e4acf79b
Yogesh Rajpurohit <jagarwalyogesh@gmail.com>
2022-07-11 14:44:20 +0530

    second commit

---
+++ Data/Assignment13Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment13.Models;
+
+namespace Assignment13.Data
+{
+    public class Assignment13Context : DbContext
+    {
+        public Assignment13Context (DbContextOptions<Assignment13Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment13.Models.Employee> Employee { get; set; }
+    }
+}
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Assignment13.Models
 {
+    [Table("Employee")]
     public class Employee
     {
+        [Key]
+        public int EId { get; set; }
+        public string EName { get; set; }
+
+        public string EDesignation { get; set; }
+
+        public DateTime? EDOJ { get; set; }
+
     }
 }
