42d233b6d05329339bd2c90a71d2b7d6c320a085
Yogesh Rajpurohit <jagarwalyogesh@gmail.com>
2022-07-11 15:00:10 +0530

    third commit

---
+++ Migrations/20220711091609_FirstMig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment13.Migrations
+{
+    public partial class FirstMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EName = table.Column<string>(nullable: true),
+                    EDesignation = table.Column<string>(nullable: true),
+                    EDOJ = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ obj/Debug/netcoreapp3.1/tid4e3xq.sql
@@ -0,0 +1,26 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Employee] (
+    [EId] int NOT NULL IDENTITY,
+    [EName] nvarchar(max) NULL,
+    [EDesignation] nvarchar(max) NULL,
+    [EDOJ] datetime2 NULL,
+    CONSTRAINT [PK_Employee] PRIMARY KEY ([EId])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220711091609_FirstMig', N'3.1.25');
+
+GO
+
